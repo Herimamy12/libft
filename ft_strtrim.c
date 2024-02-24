3ba2e9660029a6af2b563c660a8cea5681a27ea0
Herimaminavalona <herimaminavalonaprosper@gmail.com>
2024-02-24 23:09:44 +0300

    try_trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,7 +12,32 @@
 
 #include "libft.h"
 
+int	ft_countfirst(char const *s1; char const *set)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i])
+}
+
+int	ft_countlast(char const *s1; char const *set)
+{
+
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	int		lf;
+	int		ll;
+	size_t	i;
+	size_t	len;
+	char	*trim;
+
+	i = 0;
+	if (!s1 || !set)
+		return (NULL);
+	lf = ft_countfirst(char const *s1, char const *set);
+	ll = ft_countlast(char const *s1, char const *set);
+	len = ft_strlen(s1) - (lf + ll);
+	trim = (char *)malloc(sizeof(char) * len);
 }
