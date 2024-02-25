72fa46393c2fb6a28ed86d9dcf72d6fdc4bdac5c
Nirina Prosper Herimaminavalona <nherimam@c1r1s5.42antananarivo.mg>
2024-02-25 12:49:23 +0300

    modified:   ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,23 +6,64 @@
 /*   By: nherimam <nherimam@student.42antanana      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 08:35:54 by nherimam          #+#    #+#             */
-/*   Updated: 2024/02/23 08:36:21 by nherimam         ###   ########.fr       */
+/*   Updated: 2024/02/25 12:45:35 by nherimam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_countfirst(char const *s1; char const *set)
+int	ft_checkcar(char c, char const *set)
 {
 	int	i;
 
 	i = 0;
-	while (s1[i])
+	while (i < ft_strlen(set))
+	{
+		if (c == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-int	ft_countlast(char const *s1; char const *set)
+int	ft_countfirst(char const *s1, char const *set)
 {
+	int	i;
+	int	j;
+	int	count;
+	int	check;
 
+	i = 0;
+	count = 0;
+	while (i < ft_strlen(s1))
+	{
+		check = ft_checkcar(s1[i], set);
+		if (check == 0)
+			i = ft_strlen(s1);
+		count += check;
+		i++;
+	}
+	return (count);
+}
+
+int	ft_countlast(char const *s1, char const *set)
+{
+	int	i;
+	int	j;
+	int	count;
+	int	check;
+
+	count = 0;
+	i = ft_strlen(s1);
+	while (i > 0)
+	{
+		check = ft_checkcar(s1[i - 1], set);
+		if (check == 0)
+			i = 0;
+		count += check;
+		i--;
+	}
+	return (count);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
@@ -36,8 +77,15 @@ char	*ft_strtrim(char const *s1, char const *set)
 	i = 0;
 	if (!s1 || !set)
 		return (NULL);
-	lf = ft_countfirst(char const *s1, char const *set);
-	ll = ft_countlast(char const *s1, char const *set);
+	lf = ft_countfirst(s1, set);
+	ll = ft_countlast(s1, set);
 	len = ft_strlen(s1) - (lf + ll);
-	trim = (char *)malloc(sizeof(char) * len);
+	trim = (char *)malloc(sizeof(char) * (len + 1));
+	while (i < len)
+	{
+		trim[i] = s1[lf + i];
+		i++;
+	}
+	trim[i] = '\0';
+	return (trim);
 }
