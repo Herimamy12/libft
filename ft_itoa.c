5a217c2960be8f6431c98f7ac72344bece86e476
Herimaminavalona <herimaminavalonaprosper@gmail.com>
2024-02-25 20:24:52 +0300

    modifié :___ft_itoa.c___________code a revoir

--- ft_itoa.c
+++ ft_itoa.c
@@ -17,81 +17,40 @@ int	ft_count(int n)
 	int	count;
 
 	count = 0;
-	if (n < 0)
-		n *= -1;
-	while (n > 0)
-	{
+	if (n <= 0)
 		count++;
+	while (count != 0)
+	{
 		n /= 10;
+		count++;
 	}
 	return (count);
 }
 
-char	*ft_revstr(char *str)
-{
-	int		i;
-	int		len;
-	char	*dst;
-
-	i = 0;
-	len = ft_strlen(str);
-	while ((len - 1) >= 0)
-	{
-		dst[i] = str[len - 1];
-		i++;
-		len--;
-	}
-	dst[i] = '\0';
-	return (dst);
-}
-
-void	ft_negatif(int *i, int *n, int *count, char *str)
-{
-	*i++;
-	*n *= -1;
-	*count += 1;
-	str = (char *)malloc(sizeof(char) * (*count + 2));
-	str[0] = '-';
-}
-
 char	*ft_itoa(int n)
 {
-	int		i;
 	int		count;
 	char	*str;
 
-	i = 0;
-	count = 0;
 	count = ft_count(n);
-	if (n == 0 || count == 0)
-		return (0);
-	if (n < 0)
+	str = (char *)malloc(sizeof(char) * (count + 1));
+	if (str == NULL)
+		return (NULL);
+	str[count--] = '\0';
+	if (n == 0)
 	{
-		ft_negatif(&i, &n, &count, str);
-/*		i++;
-		n *= -1;
-		count += 1;
-		str = (char *)malloc(sizeof(char) * (count + 2));
-		str[0] = '-';	*/
+		str[0] = '0';
+		return (str);
 	}
-	else
-		str = (char *)malloc(sizeof(char) * (count + 1));
+	if (n < 0)
+		str[0] = '-';
 	while (n != 0)
 	{
-		str[i] = (n % 10) + 48;
-		n = n / 10;
-		i++;
+		if (str[0] == '-')
+			str[count--] = '0' + -(n % 10);
+		else
+			str[count--] = '0' + (n % 10);
+		n /= 10;
 	}
-	str[i] = '\0';
-	str = ft_revstr(str);
 	return (str);
-}
-
-int main()
-{
-    int i = 123;
-    char    *str;
-    
-    str = ft_itoa(i);
-    printf ("%s", str);
 }
