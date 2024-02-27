509fcab982465ef24d7fe4d8004be0314131d221
Herimaminavalona <herimaminavalonaprosper@gmail.com>
2024-02-27 23:19:51 +0300

    list_chainée

--- libft.h
+++ libft.h
@@ -58,5 +58,7 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
